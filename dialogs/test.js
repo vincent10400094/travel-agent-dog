dacd0a357f73d180e23f91dd41a0182537fbf8e8
ChunYaoChang <b07902022@csie.ntu.edu.tw>
2021-01-29 23:23:20 +0800

    update

--- dialogs/test.js
+++ dialogs/test.js
@@ -1,4 +1,13 @@
 const getRoute = require('./getRoute').getRoute;
 attractions = ['台北霞海城隍廟', '國父紀念館', '南門市場'];
 
-getRoute(attractions, '關渡自然公園');
+
+function scheduleCard(attractions, startPoint) {
+    var data = getRoute(attractions, startPoint);
+    var template = new ACData.Template(templateCard);
+    var cardPayload = template.expand({
+        $root: data
+    });
+    var adaptiveCard = CardFactory.adaptiveCard(cardPayload);
+    return adaptiveCard;
+}
--- dialogs/utility.js
+++ dialogs/utility.js
@@ -1,6 +1,8 @@
 const ACData = require("adaptivecards-templating");
 const PlaceCard = require('../data/tourist_spots');
-const templateCard = require('../templates/recommendCardTemplate');
+const recommendCardTemplate = require('../templates/recommendCardTemplate');
+const scheduleCardTemplate = require('../templates/scheduleCardTemplate');
+const getRoute = require('./getRoute').getRoute;
 
 function findSpot(title) {
     for (let i in PlaceCard) {
@@ -14,7 +16,7 @@ function findSpot(title) {
 
 function generateCard(title) {
     var data = findSpot(title);
-    var template = new ACData.Template(templateCard);
+    var template = new ACData.Template(recommendCardTemplate);
     var cardPayload = template.expand({
         $root: data
     });
@@ -23,5 +25,20 @@ function generateCard(title) {
 
 }
 
+function scheduleCard(attractions, startPoint) {
+    var data = getRoute(attractions, startPoint);
+    var cardArray = [];
+    for (let i of data) {
+        var template = new ACData.Template(scheduleCardTemplate);
+        var cardPayload = template.expand({
+            $root: i
+        });
+        var adaptiveCard = CardFactory.adaptiveCard(cardPayload);
+        cardArray.push(adaptiveCard);
+    }
+    return cardArray;
+}
+
 module.exports.generateCard = generateCard;
 module.exports.findSpot = findSpot;
+module.exports.scheduleCard = scheduleCard;
